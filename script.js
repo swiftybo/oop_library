ffd326a74c01649d74810e7611b7ed4d006d965a
swiftybo <leungjordan28@gmail.com>
2025-06-12 16:15:24 +0100

    es6 class created and function to add book to library array

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 "use strict";
 
+const myLibrary = [];
+
 class Book {
     constructor(title, author, pages, read) {
         this.title = title;
         this.author = author;
         this.pages = pages;
-        this.read = read;
     }
 
-    info() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+    info(read) {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${read}`;
     }
 }
 
-const hobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "not read");
-console.log(hobbit.info());
+const addBookTolibrary = function (title, author, pages) {
+    const newNovel = new Book(title, author, pages);
+    newNovel.id = crypto.randomUUID();
+    myLibrary.push(newNovel);
+};
+
+addBookTolibrary("The Hobbit", "J.R.R Tolkien", 295);
+addBookTolibrary("Gone", "Michael Grant", 315);
+addBookTolibrary("The Kite Runner", "Matthew Spangler", 180);
+addBookTolibrary("The Alchemist", "Paulo Coelho", 165);
+addBookTolibrary(
+    "Harry Potter and the Order of the Phoenix",
+    "JK Rowling",
+    870
+);
