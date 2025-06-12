3e74c3311addd34e1748b9c5701fad31eb1fa022
swiftybo <leungjordan28@gmail.com>
2025-06-12 15:51:58 +0100

    Added book class and first instance

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+
+    info() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+    }
+}
+
+const hobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "not read");
+console.log(hobbit.info());
